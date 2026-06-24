1adafa6ceed7f8b398fffc37ae7692a05642b4f6
Willsky013 <13williamfb@gmail.com>
2026-06-24 12:29:10 +0200

    chanched text in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 export default function Home() {
   return (
     <main className="home">
-      <div id="hero">
-        <h1>Discover the best skateparks around the world</h1>
-        <p>Your travel guide to amazing skateparks.</p>
-        <p>Find, explore and save your favorites.</p>
 
-      </div>
-      <div id="popular-Skateparks">
-        <h2>Popular Skateparks</h2>
+      <section id="hero">
+        <div id="hero-Text">
 
-      </div>
+
+        </div>
+        <div id="popular-Skateparks">
+
+
+        </div>
+      </section>
+
       <div id="browse-Skateparks">
-        <h2>Browse Skateparks</h2>
+
 
       </div>
     </main>
