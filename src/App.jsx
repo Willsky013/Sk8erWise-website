3e08488a5485ef91de433b2aa1464c99400d604a
Willsky013 <13williamfb@gmail.com>
2026-06-24 12:28:14 +0200

    added structur to home page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         {/* <Route path="*" element={<Navigate to="/" />} /> */}
       </Routes>
+
       <Footer />
+      
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
 export default function Home() {
   return (
-    <main className="home"></main>
+    <main className="home">
+      <div id="hero">
+        <h1>Discover the best skateparks around the world</h1>
+        <p>Your travel guide to amazing skateparks.</p>
+        <p>Find, explore and save your favorites.</p>
+
+      </div>
+      <div id="popular-Skateparks">
+        <h2>Popular Skateparks</h2>
+
+      </div>
+      <div id="browse-Skateparks">
+        <h2>Browse Skateparks</h2>
+
+      </div>
+    </main>
   )
 }
