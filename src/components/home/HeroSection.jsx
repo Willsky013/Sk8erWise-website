fbcf34b296006e6cc845b3d5125141c2f4712e24
idesignerSe <hebera75@gmail.com>
2026-06-26 13:09:32 +0200

    Home.jsx from divs to Components

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,13 @@
+import '../components-css/Herosection.css';
+
+export default function HeroSection() {
+  return (
+    <section id="hero">
+      <div id="hero-Text">
+        <h1 id='welcome-Text'>Discover the best skateparks around the world</h1>
+        <p>Your travel guide to amazing skateparks.</p>
+        <p>Find, explore and save your favorites</p>
+      </div>
+    </section>
+  );
+}
