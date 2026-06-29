3ecb89c776adfb4044ebc6b3e3fa6a8d29a8756f
William <13williamfb@gmail.com>
2026-06-29 13:24:28 +0200

    Update image source in ParkCard component

--- src/components/ParkCard.jsx
+++ src/components/ParkCard.jsx
@@ -24,7 +24,7 @@ export default function ParkCard({ park }) {
     return(
     <div className = "parkCard">
         <FavoriteButton parkId={park.id} />
-        <img src = {park.images[0]} alt = "Här ska kortet sitta" className = "parkImage" />
+        <img src={`${import.meta.env.BASE_URL}${park.images[0]}`} alt={park.name} id="parkImage" />
         <h2>{park.name}</h2>
         <h3>{park.city}, {park.country}</h3>
         <div className = "inforow">
