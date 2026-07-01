109e662cea5b44b663fd563cb26364bf02e9f5ac
HaidarAlhaidari <alhaidari1050@gmail.com>
2026-07-01 13:51:43 +0200

    ändrar font

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -5,8 +5,10 @@ export default function HeroSection() {
     <section id="hero">
       <div id="hero-Text">
         <h1 id='welcome-Text'>Discover the best skateparks around the world</h1>
+        <div  id="hero-Subtext">
         <p>Your travel guide to amazing skateparks.</p>
         <p>Find, explore and save your favorites</p>
+        </div>
       </div>
     </section>
   );
