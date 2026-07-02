fa63bd3ff449294bcf57091eeff88d60d6008abe
Willsky013 <13williamfb@gmail.com>
2026-07-02 09:50:46 +0200

    wrong

--- src/components/ParkDetailModal.jsx
+++ src/components/ParkDetailModal.jsx
@@ -4,35 +4,71 @@ import ImageSlider from "./ImageSlider";
 function ParkDetailModal({ park, onClose }) {
   if (!park) return null;
 
-  // const image =
-  //   park.images?.[0]
-  //     ? `${import.meta.env.BASE_URL}${park.images[0].replace(/^\//, "")}`
-  //     : park.image;
+  const images =
+    park.images?.map(
+      (img) => `${import.meta.env.BASE_URL}${img.replace(/^\//, "")}`
+    ) || [park.image];
 
-  const images = park.images?.map((img) => `${import.meta.env.BASE_URL}${img.replace(/^\//, "")}`) || [park.image];
-
-  console.log(images);
 
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="park-detail-modal" onClick={(e) => e.stopPropagation()}>
-        <button className="modal-close-btn" onClick={onClose}>
-          ×
-        </button>
 
-        <ImageSlider images={images} />
+      <div 
+        className="park-detail-modal" 
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button 
+          className="modal-close-btn" 
+          onClick={onClose}
+        >×</button>
 
-        <h2>{park.name}</h2>
-        <p>{park.description}</p>
+        {/* IMAGE AREA */}
+        <div className="image-container">
+          <ImageSlider images={images} />
+        </div>
 
-        <h3>Amenities</h3>
-        <ul>
-          {(park.amenities || []).map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
 
-        <p>Rating: ⭐ {park.rating}</p>
+        {/* CONTENT */}
+        <div className="park-content">
+
+          <h2>{park.name}</h2>
+
+          <span className="location">
+            {park.city}, {park.country} 
+          </span>
+
+          <p className="description">{park.description}</p>
+
+          <h3>AMENITIES</h3>
+
+          <ul className="amenities">
+            {(park.amenities || []).map((item) => (
+              <li key={item}>
+                <span className="amenity-icon">
+                  ○
+                </span>
+                {item}
+              </li>
+            ))}
+          </ul>
+
+          <div className="bottom-section">
+
+            <span className="difficulty">
+              {park.difficulty}
+            </span>
+
+            <div className="rating">
+
+              <span>
+                RATING:
+              </span>
+
+              ⭐ {park.rating}
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
